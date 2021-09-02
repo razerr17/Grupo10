5a6bf377760853c19d3e3b78574d7f63e0987600
Edgar-PC <razerrdark17@gmail.com>
2021-09-02 00:26:01 -0500

    Actualizacion BackendEstudiante
    se agrego la funcionalidad del atributo semestreingreso,se restringio la funcion update de la api de estudiantes para solo los atributos email,direccion y celular,tambien se retiro el backend de tutor

--- src/database/querys.js
+++ src/database/querys.js
@@ -1,20 +1,14 @@
 export const queries={
     // Queries Estudiantes
     getAllEstudiantes:"Select * from TEstudiante",
-    getEstudianteById:"Select * from TEstudiante where IDEstudiante=@IDEstudiante",
-    addNewEstudiantes:"Insert into TEstudiante Values (@IDEstudiante,@Nombres,@ApPaterno,@ApMaterno,@Email,@Direccion,@Celular)",
-    deleteEstudianteById:"delete from TEstudiante where IDEstudiante=@IDEstudiante",
-    updateEstudianteById:"update TEstudiante set IDEstudiante=@IDEstudiante,Nombres=@Nombres,ApPaterno=@ApPaterno,ApMaterno=@ApMaterno,Email=@Email,Direccion=@Direccion,Celular=@Celular where IDEstudiante=@IDEstudiante",
+    getEstudianteById:"Select * from TEstudiante where CodEstudiante=@CodEstudiante",
+    addNewEstudiantes:"Insert into TEstudiante Values (@CodEstudiante,@Nombres,@ApPaterno,@ApMaterno,@Email,@Direccion,@Celular,@SemestreIngreso)",
+    deleteEstudianteById:"delete from TEstudiante where CodEstudiante=@CodEstudiante",
+    updateEstudianteById:"update TEstudiante set Email=@Email,Direccion=@Direccion,Celular=@Celular where CodEstudiante=@CodEstudiante",
     // Queries Docentes
     getAllDocentes:"Select * from TDocente",
     getDocenteById:"Select * from TDocente where IDDocente=@IDDocente",
     addNewDocente:"Insert into TDocente Values (@IDDocente,@Nombre,@DNI,@Correo,@Celular,@Direccion)",
     deleteDocenteById:"delete from TDocente where IDDocente=@IDDocente",
-    updateDocenteById:"update TDocente set Nombre=@Nombre,DNI=@DNI,Correo=@Correo,Celular=@Celular,Direccion=@Direccion where IDDocente=@IDDocente",
-    // Queries Tutor
-    getAllTutor:"Select * from TTutor",
-    getTutorById:"Select * from TTutor where IDTutor=@IDTutor",
-    addNewTutor:"Insert into TTutor Values (@IDTutor,@Tutorados,@IDDocente)",
-    deleteTutorById:"delete from TTutor where IDTutor=@IDTutor",
-    updateTutorById:"update TTutor set IDTutor=@IDTutor,Tutorados=@Tutorados,IDDocente=@IDDocente where IDTutor=@IDTutor"
+    updateDocenteById:"update TDocente set Nombre=@Nombre,DNI=@DNI,Correo=@Correo,Celular=@Celular,Direccion=@Direccion where IDDocente=@IDDocente"
 }
